52be83c339634eb41b301608976dc74523a79c56
InigoHeredia12 <0242573@up.edu.mx>
2024-08-20 19:18:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 
-# Título de la aplicación
 st.title('Currículum Iñigo Heredia Hernandez')
 
-# Sección de introducción
 st.header('Introducción')
 st.write('Soy una persona trabajadora, apasionada por el deporte y muy sociable. Disfruto de los desafíos y me encanta rodearme de gente positiva.')
 
-# Crear dos columnas para dividir el contenido
 col1, col2 = st.columns(2)
 
 with col1:
@@ -16,18 +13,21 @@ with col1:
     st.write('Tribu Living 2022 - 2023')
     st.write('Cb Espacios 2023 - 2024')
 
-    st.header('Educación')
-    st.write('Liceo del Valle 2009 - 2021')
-    st.write('Universidad Panamericana 2021 - Ahora')
+    st.header('Experiencia Profesional')
+    st.write('Afix Financiera 2021 - 2022')
+    st.write('Tribu Living 2022 - 2023')
+    st.write('Cb Espacios 2023 - 2024')
 
 with col2:
-    st.header('Educación')
-    st.write('Liceo del Valle 2009 - 2021')
-    st.write('Universidad Panamericana 2021 - Ahora')
 
-# Sección de habilidades
 st.header('Habilidades')
-st.write('Aquí puedes listar tus habilidades técnicas y blandas.')
+st.write('Resolución de problemas: Habilidad para encontrar soluciones creativas e innovadoras a desafíos.
+Ejemplo: Si se presenta un imprevisto durante un evento social, puedes pensar rápidamente en alternativas para mantener a todos contentos.
+Aprendizaje rápido: Capacidad de adquirir nuevos conocimientos y habilidades de manera eficiente.
+Ejemplo: Puedes aprender a usar un nuevo programa informático en poco tiempo y con facilidad.
+Adaptabilidad: Flexibilidad para ajustarte a nuevas situaciones y cambios.
+Ejemplo: Puedes cambiar tus planes de manera espontánea si surge una mejor oportunidad.')
+
 
 # Sección de logros
 st.header('Logros')
